80a9898c6ab222d61e2b27a389d9650890b1e905
08fdange <08fdange@gmail.com>
2021-01-02 13:48:04 -0500

    Initial commit with working Node.js/MongoDB API w/ User model

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+// contactModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+// Export Contact model
+var User = module.exports = mongoose.model('user', userSchema);
+module.exports.get = function (callback, limit) {
+    User.find(callback).limit(limit);
+}
